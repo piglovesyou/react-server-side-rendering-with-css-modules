277b397f217def5354a4d0ae59c8786384c188e7
piglovesyou <thepiglovesyou@gmail.com>
2017-06-10 21:40:41 +0900

    test action

--- src/client/components/home/index.js
+++ src/client/components/home/index.js
@@ -1,10 +1,11 @@
 const React = require('react');
 const s = require('./index.sass');
+const {baam} =  require('../../actions');
 
 module.exports.default = function Home(props) {
   return (
     <div className={s.root}>
-      <h2>Home...</h2>
+      <h2>Home... <button onClick={baam}>Add message</button></h2>
       <h3>Messages</h3>
       <ul>
         {props.messages.map((m, i) => <li key={i}>{m}</li>)}
